c45520c949320df1ebfa11640354d27db3a4b508
Robertarg72 <robertarg72@gmail.com>
2018-01-02 00:58:17 -0500

    Values setup for each table view cell. Fix bug: cells got disabled when scrolling.

--- TodoListApp/RootViewController.swift
+++ TodoListApp/RootViewController.swift
@@ -64,7 +64,7 @@ class TaskViewCell: UITableViewCell {
 }
 
 // Main controller for first table view showing list of taks
-class RootViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class RootViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UIScrollViewDelegate {
     
     // Private variables
     private var cellPointSize: CGFloat!
@@ -72,6 +72,22 @@ class RootViewController: UIViewController, UITableViewDataSource, UITableViewDe
     private static let taskCell = "TaskName"
     @IBOutlet weak var todoListTableView: UITableView!
     
+    // Private constants for setting up color used in this screen
+    let tintColorForNavigationBar = UIColor(red: Utils.rgbHexaComponentToDecimal("DD"),
+                                            green: Utils.rgbHexaComponentToDecimal("2D"),
+                                            blue: Utils.rgbHexaComponentToDecimal("01"), alpha: 1)
+    
+    let disabledColorForName = UIColor(red: Utils.rgbHexaComponentToDecimal("E1"),
+                                       green: Utils.rgbHexaComponentToDecimal("E2"),
+                                       blue: Utils.rgbHexaComponentToDecimal("E1"), alpha: 1)
+    
+    let enabledColorForName = UIColor(red: Utils.rgbHexaComponentToDecimal("61"),
+                                    green: Utils.rgbHexaComponentToDecimal("61"),
+                                    blue: Utils.rgbHexaComponentToDecimal("61"), alpha: 1)
+    
+    let enabledColorForEditButtonText = UIColor(red: Utils.rgbHexaComponentToDecimal("A0"),
+                                             green: Utils.rgbHexaComponentToDecimal("00"),
+                                             blue: Utils.rgbHexaComponentToDecimal("00"), alpha: 1)
     
     // OVERRIDDE FUNCTIONS FOR UIVIEWCONTROLLER PROTOCOL
     
@@ -84,35 +100,32 @@ class RootViewController: UIViewController, UITableViewDataSource, UITableViewDe
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
+        todoListTableView!.reloadData()
         //self.navigationController!.navigationBar.frame = CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: 800.0)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        // Set up the singleton shared list of tasks
         tasksList = TasksList.sharedTasksList
+        
+        // Set up the font attributes, tint color, and title position for the navigation bar
         let  preferredTableViewFont =  UIFont.preferredFont(forTextStyle: UIFontTextStyle.headline)
         cellPointSize = preferredTableViewFont.pointSize
-        //tableView.estimatedRowHeight = cellPointSize
         todoListTableView!.estimatedRowHeight = cellPointSize
-        
-        navigationController?.navigationBar.barTintColor = UIColor(
-                        red: Utils.rgbHexaComponentToDecimal("DD"),
-                        green: Utils.rgbHexaComponentToDecimal("2D"),
-                        blue: Utils.rgbHexaComponentToDecimal("01"), alpha: 1)
-        
+        navigationController?.navigationBar.barTintColor = tintColorForNavigationBar
         navigationController?.navigationBar.tintColor = UIColor.white
         
         let attrs = [
             NSAttributedStringKey.foregroundColor: UIColor.white,
             NSAttributedStringKey.font: UIFont.systemFont(ofSize: 26, weight: .semibold)
         ]
-        
         navigationController?.navigationBar.titleTextAttributes = attrs
         navigationController?.navigationBar.setTitleVerticalPositionAdjustment(CGFloat(-10), for: UIBarMetrics.default)
         
+        // Modify the text for the default "Back Button" in the navigation bar
         navigationItem.backBarButtonItem = UIBarButtonItem(title: "Back", style: .plain, target: nil,  action: nil)
-       
     }
   
     
@@ -121,28 +134,30 @@ class RootViewController: UIViewController, UITableViewDataSource, UITableViewDe
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return tasksList.tasks.count
     }
-   
-    func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
-        return tasksList.tasks[indexPath.row].completed ? nil : indexPath
-    }
-
+    
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return UIFont.preferredFont(forTextStyle: UIFontTextStyle.headline).pointSize * 5
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = todoListTableView!.dequeueReusableCell(withIdentifier: RootViewController.taskCell, for: indexPath) as! TaskViewCell
-        
         cell.name?.text = tasksList.tasks[indexPath.row].name
-        cell.row = indexPath.row
+        cell.row = indexPath.row // Save cell row position in the table view, for updating each task when switch changed on event happens
+        
+        // Set up status, task name font color, and edit button text color according to data stored in the TasksList shared singleton
         if tasksList.tasks[indexPath.row].completed {
             cell.completed?.isOn = false
-            cell.completed?.isEnabled = false
-            cell.editButton?.isEnabled = false
+            //cell.completed?.isEnabled = false
+            //cell.editButton?.isEnabled = false
             cell.editButton?.setTitleColor(.gray, for: .normal)
-            cell.name?.textColor = UIColor(red: Utils.rgbHexaComponentToDecimal("E1"),
-                                           green: Utils.rgbHexaComponentToDecimal("E2"),
-                                           blue: Utils.rgbHexaComponentToDecimal("E1"), alpha: 1)
+            cell.name?.textColor = disabledColorForName
+        }
+        else {
+            cell.completed?.isOn = true
+            cell.completed?.isEnabled = true
+            cell.editButton?.isEnabled = true
+            cell.editButton?.setTitleColor(enabledColorForEditButtonText, for: .normal)
+            cell.name?.textColor = enabledColorForName
         }
         
         // Set button tag to the cell order row, so we have that info for the segue action
